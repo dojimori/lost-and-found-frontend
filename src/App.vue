6fd6d0c0ddf49ea8c354d70e681e36968ce9b683
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-19 23:45:34 +0800

    some fade animations

--- src/App.vue
+++ src/App.vue
@@ -46,12 +46,12 @@
 
   <div class="hero bg-base-200 min-h-[86vh]">
     <div class="hero-content text-center">
-      <div class="max-w-md">
+      <div class="max-w-md animate-fade">
         <h1 class="text-5xl font-bold">Found something in the campus?</h1>
         <p class="py-6">
           A simple place where seekers and finders meet; bring lost items back home!
         </p>
-        <button class="btn btn-primary">Get Started</button>
+        <button class="btn btn-primary animate-bounce ">Get Started</button>
       </div>
     </div>
   </div>
@@ -59,7 +59,23 @@
 
 </template>
 
-<style scoped></style>
+<style scoped>
+.animate-fade {
+  animation: fade 1s;
+}
+
+@keyframes fade {
+  from {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+}
+</style>
 <script>
 import { PhCheckCircle, PhChartBarHorizontal, PhChecks } from '@phosphor-icons/vue'
 
