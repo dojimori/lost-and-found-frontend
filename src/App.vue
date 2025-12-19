2131a2be481fbdc2cf0c38dfce699b2c026394d6
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-19 23:38:56 +0800

    hero

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,73 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="drawer lg:drawer-open">
-    <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content">
-      <!-- Navbar -->
-      <nav class="navbar w-full bg-base-300">
-        <label for="my-drawer-4" aria-label="open sidebar" class="btn btn-square btn-ghost">
-          <!-- Sidebar toggle icon -->
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round" stroke-linecap="round"
-            stroke-width="2" fill="none" stroke="currentColor" class="my-1.5 inline-block size-4">
-            <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
-            <path d="M9 4v16"></path>
-            <path d="M14 10l2 2l-2 2"></path>
+  <nav class="navbar bg-base-100 shadow-sm">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
           </svg>
-        </label>
-        <div class="px-4">Organizado</div>
-      </nav>
-      <!-- Page content here -->
-      <div class="p-4">Page Content</div>
-    </div>
-
-    <div class="drawer-side is-drawer-close:overflow-visible">
-      <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
-      <div class="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
-        <!-- Sidebar content here -->
-        <ul class="menu w-full grow">
-          <!-- List item -->
-          <li>
-            <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Homepage">
-              <!-- Home icon -->
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round" stroke-linecap="round"
-                stroke-width="2" fill="none" stroke="currentColor" class="my-1.5 inline-block size-4">
-                <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
-                <path
-                  d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z">
-                </path>
-              </svg>
-              <span class="is-drawer-close:hidden">Homepage</span>
-            </button>
-          </li>
-
-          <!-- List item -->
-          <li>
-            <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Settings">
-              <!-- Settings icon -->
-              <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round" stroke-linecap="round"
-                stroke-width="2" fill="none" stroke="currentColor" class="my-1.5 inline-block size-4">
-                <path d="M20 7h-9"></path>
-                <path d="M14 17H5"></path>
-                <circle cx="17" cy="17" r="3"></circle>
-                <circle cx="7" cy="7" r="3"></circle>
-              </svg> -->
-              <ph-check-circle :size="12" class="my-1.5 inline-block size-4"></ph-check-circle>
-              <span class="is-drawer-close:hidden font-bold text-lg">My Tasks</span>
-            </button>
-          </li>
+        </div>
+        <ul tabindex="-1" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+          <li><a>Item 1</a></li>
+          <!-- <li>
+            <a>Parent</a>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </li> -->
+          <li><a>Item 3</a></li>
         </ul>
+      </div>
+      <a class="btn btn-ghost text-xl">Seekr</a>
+    </div>
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        <li><a>About</a></li>
+        <!-- <li>
+          <details>
+            <summary>Parent</summary>
+            <ul class="p-2 bg-base-100 w-40 z-1">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </details>
+        </li> -->
+        <li><a>Developer</a></li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+      <a class="btn mr-2">Login</a>
+      <a class="btn">Register</a>
+    </div>
+  </nav>
+
+  <div class="hero bg-base-200 min-h-[86vh]">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Found something in the campus?</h1>
+        <p class="py-6">
+          A simple place where seekers and finders meet; bring lost items back home!
+        </p>
+        <button class="btn btn-primary">Get Started</button>
       </div>
     </div>
   </div>
+
+
 </template>
 
 <style scoped></style>
 <script>
-import { PhCheckCircle } from '@phosphor-icons/vue'
+import { PhCheckCircle, PhChartBarHorizontal, PhChecks } from '@phosphor-icons/vue'
 
 export default {
   components: {
-    PhCheckCircle
+    PhCheckCircle,
+    PhChecks,
+    PhChartBarHorizontal
   }
 }
 
