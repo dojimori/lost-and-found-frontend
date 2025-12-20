b7b3b8e119e4e6feec8095027985ab2103913fbd
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-20 23:06:02 +0800

    confirm btn

--- src/views/authed/claims-view.vue
+++ src/views/authed/claims-view.vue
@@ -11,17 +11,45 @@
                     <th>Item</th>
                     <th>Image</th>
                     <th>Claimed date</th>
+                    <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
 
                 <tr>
-                    <td>Mystery Box</td>
+                    <td>Najid Badji Ali</td>
                     <td>Wallet</td>
                     <td><img src="https://scontent.fcgy1-1.fna.fbcdn.net/v/t39.30808-6/598274256_867114656251812_6662297040089705085_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeEyYvcKYon9WtZ0BIpckYWRWpVuJk4OEElalW4mTg4QSR-UQEzlkmKnTtVf-o3OeuEjmg4G-Tunx0-P_CCrLLi8&_nc_ohc=AZcgVEQu9wIQ7kNvwFTfsgm&_nc_oc=AdmmlJo7RQxwckjwxXeKS95nR7Ttfuv7uUk6IydEIxMtmMu1EEGLxZEp2MQ232l9hhA&_nc_zt=23&_nc_ht=scontent.fcgy1-1.fna&_nc_gid=n0E2T-ez710J8-tiYNDw_g&oh=00_Afnbt2VNgygaVjggaA9ihMmlpqkmzKpBD8JpfREDPCxbIg&oe=694BCDC0"
                             class="max-w-[50px] rounded-md object-cover" />
                     </td>
                     <td>6/26/2026</td>
+                    <td>
+                        <button class="btn btn-success">Confirm</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Najid Badji Ali</td>
+                    <td>Wallet</td>
+                    <td><img src="https://scontent.fcgy1-1.fna.fbcdn.net/v/t39.30808-6/598274256_867114656251812_6662297040089705085_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeEyYvcKYon9WtZ0BIpckYWRWpVuJk4OEElalW4mTg4QSR-UQEzlkmKnTtVf-o3OeuEjmg4G-Tunx0-P_CCrLLi8&_nc_ohc=AZcgVEQu9wIQ7kNvwFTfsgm&_nc_oc=AdmmlJo7RQxwckjwxXeKS95nR7Ttfuv7uUk6IydEIxMtmMu1EEGLxZEp2MQ232l9hhA&_nc_zt=23&_nc_ht=scontent.fcgy1-1.fna&_nc_gid=n0E2T-ez710J8-tiYNDw_g&oh=00_Afnbt2VNgygaVjggaA9ihMmlpqkmzKpBD8JpfREDPCxbIg&oe=694BCDC0"
+                            class="max-w-[50px] rounded-md object-cover" />
+                    </td>
+                    <td>6/26/2026</td>
+                    <td>
+                        <button class="btn btn-success">Confirm</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Najid Badji Ali</td>
+                    <td>Wallet</td>
+                    <td><img src="https://scontent.fcgy1-1.fna.fbcdn.net/v/t39.30808-6/598274256_867114656251812_6662297040089705085_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeEyYvcKYon9WtZ0BIpckYWRWpVuJk4OEElalW4mTg4QSR-UQEzlkmKnTtVf-o3OeuEjmg4G-Tunx0-P_CCrLLi8&_nc_ohc=AZcgVEQu9wIQ7kNvwFTfsgm&_nc_oc=AdmmlJo7RQxwckjwxXeKS95nR7Ttfuv7uUk6IydEIxMtmMu1EEGLxZEp2MQ232l9hhA&_nc_zt=23&_nc_ht=scontent.fcgy1-1.fna&_nc_gid=n0E2T-ez710J8-tiYNDw_g&oh=00_Afnbt2VNgygaVjggaA9ihMmlpqkmzKpBD8JpfREDPCxbIg&oe=694BCDC0"
+                            class="max-w-[50px] rounded-md object-cover" />
+                    </td>
+                    <td>6/26/2026</td>
+                    <td>
+                        <button class="btn btn-success">Confirm</button>
+                    </td>
                 </tr>
 
             </tbody>
