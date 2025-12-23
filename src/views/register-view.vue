f0fcb438f309a2b673f4ca417b1fdb7cc63abc60
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-23 22:45:32 +0800

    lil alert border

--- src/views/register-view.vue
+++ src/views/register-view.vue
@@ -1,123 +1,95 @@
 <template>
-  <div class="flex items-center p-4 justify-center h-[80vH]" v-motion-fade>
-    <form
-      @submit.prevent="submitHandler"
-      class="flex flex-col gap-1 w-100 p-4 border border-gray-300 shadow-lg"
-    >
-      <h4 class="font-bold">Register</h4>
+    <div class="flex items-center p-4 justify-center h-[80vH]" v-motion-fade>
+        <form @submit.prevent="submitHandler" class="flex flex-col gap-1 w-100 p-4 border border-gray-300 shadow-lg">
+            <h4 class="font-bold">Register</h4>
 
-      <div class="flex flex-col gap-2">
-        <label for="" class="floating-label">
-          <span>Name</span>
-          <input
-            v-model="credentials.name"
-            type="text"
-            class="input w-full"
-            placeholder="Name"
-            :disabled="isLoading"
-          />
-        </label>
+            <div class="flex flex-col gap-2">
+                <label for="" class="floating-label">
+                    <span>Name</span>
+                    <input v-model="credentials.name" type="text" class="input w-full" placeholder="Name"
+                        :disabled="isLoading" />
+                </label>
 
-        <label for="" class="floating-label">
-          <span>Email</span>
-          <input
-            v-model="credentials.email"
-            type="text"
-            class="input w-full"
-            placeholder="Email"
-            :disabled="isLoading"
-          />
-        </label>
+                <label for="" class="floating-label">
+                    <span>Email</span>
+                    <input v-model="credentials.email" type="text" class="input w-full" placeholder="Email"
+                        :disabled="isLoading" />
+                </label>
 
-        <label for="" class="floating-label">
-          <span>Password</span>
-          <input
-            v-model="credentials.password"
-            type="text"
-            class="input w-full"
-            placeholder="Password"
-            :disabled="isLoading"
-          />
-        </label>
+                <label for="" class="floating-label">
+                    <span>Password</span>
+                    <input v-model="credentials.password" type="text" class="input w-full" placeholder="Password"
+                        :disabled="isLoading" />
+                </label>
 
-        <label for="" class="floating-label">
-          <span>Confirm Password</span>
-          <input
-            v-model="credentials.confirmPassword"
-            type="text"
-            class="input w-full"
-            placeholder="Confirm Password"
-            :disabled="isLoading"
-          />
-        </label>
-      </div>
+                <label for="" class="floating-label">
+                    <span>Confirm Password</span>
+                    <input v-model="credentials.confirmPassword" type="text" class="input w-full"
+                        placeholder="Confirm Password" :disabled="isLoading" />
+                </label>
+            </div>
 
-      <button type="submit" class="btn btn-primary mt-4" :disabled="isLoading">
-        <span v-if="isLoading" class="loading loading-dots loading-lg"></span>
-        <span v-else>Register</span>
-      </button>
-      <!-- mt-2 alert alert-error alert-soft -->
-      <div
-        v-motion-fade
-        role="alert"
-        v-if="showAlert"
-        :class="[
-          'mt-2 alert alert-soft',
-          { 'alert-error': isError, 'alert-success': !isError },
-        ]"
-      >
-        <span>{{ responseMessage }}</span>
-      </div>
-    </form>
-  </div>
+            <button type="submit" class="btn btn-primary mt-4" :disabled="isLoading">
+                <span v-if="isLoading" class="loading loading-dots loading-lg"></span>
+                <span v-else>Register</span>
+            </button>
+            <!-- mt-2 alert alert-error alert-soft -->
+            <div v-motion-fade role="alert" v-if="showAlert" :class="[
+                'mt-2 alert alert-soft',
+                { 'alert-error border border-red-200': isError, 'alert-success border border-green-200': !isError },
+            ]">
+                <span>{{ responseMessage }}</span>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
 import { api } from "../helpers/api";
 
 export default {
-  data() {
-    return {
-      isLoading: false,
-      isError: false,
-      showAlert: false,
-      responseMessage: "",
-      credentials: {
-        name: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-      },
-    };
-  },
-
-  methods: {
-    async submitHandler() {
-      try {
-        this.isLoading = true;
-        this.isError = false;
-        this.showAlert = false;
-        if (this.credentials.password != this.credentials.confirmPassword) {
-          this.isError = true;
-          this.responseMessage = "Password does not match.";
-          return;
-        }
-
-        const response = await api.post("/auth/register", this.credentials);
-
-        // console.log(response);
-        this.responseMessage = response.data.message;
-      } catch (error) {
-        this.isError = true;
-        console.log(error);
-        const message =
-          error.response.data.message || "Something wen't terribly wrong here.";
-        this.responseMessage = message;
-      } finally {
-        this.isLoading = false;
-        this.showAlert = true;
-      }
+    data() {
+        return {
+            isLoading: false,
+            isError: false,
+            showAlert: false,
+            responseMessage: "",
+            credentials: {
+                name: "",
+                email: "",
+                password: "",
+                confirmPassword: "",
+            },
+        };
     },
-  },
+
+    methods: {
+        async submitHandler() {
+            try {
+                this.isLoading = true;
+                this.isError = false;
+                this.showAlert = false;
+                if (this.credentials.password != this.credentials.confirmPassword) {
+                    this.isError = true;
+                    this.responseMessage = "Password does not match.";
+                    return;
+                }
+
+                const response = await api.post("/auth/register", this.credentials);
+
+                // console.log(response);
+                this.responseMessage = response.data.message;
+            } catch (error) {
+                this.isError = true;
+                console.log(error);
+                const message =
+                    error.response.data.message || "Something wen't terribly wrong here.";
+                this.responseMessage = message;
+            } finally {
+                this.isLoading = false;
+                this.showAlert = true;
+            }
+        },
+    },
 };
 </script>
