02e7d0736e6cbac393666088200eba4e6c3e0221
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-23 21:53:59 +0800

    restyle: login page

--- src/views/login-view.vue
+++ src/views/login-view.vue
@@ -1,31 +1,30 @@
 <template>
-    <div class="flex items-center p-4 justify-center h-[80vH] " v-motion-fade>
+  <div class="flex items-center p-4 justify-center h-[80vH]" v-motion-fade>
+    <form action="" class="flex flex-col w-100 p-4 border shadow-lg border-gray-300">
+      <h4 class="mb-4 font-bold">Login</h4>
+      <div class="flex flex-col gap-2">
+        <label for="" class="floating-label">
+          <span>Email</span>
+          <input type="text" class="input w-full" placeholder="Email" />
+        </label>
 
-        <form action="" class="flex flex-col w-100 bg-gray-100  p-4 border border-gray-300">
-            <h4 class="mb-4 font-bold">Login</h4>
-            <label for="" class="floating-label">
-                <span>Email</span>
-                <input type="text" class="input w-full " placeholder="Email">
-            </label>
+        <label class="floating-label">
+          <span>Password</span>
+          <input type="text" class="input w-full" placeholder="Password" />
+        </label>
+      </div>
 
-            <label class="floating-label  mt-2">
-                <span>Password</span>
-                <input type="text" class="input w-full" placeholder="Password">
-            </label>
-
-            <button type="submit" class="btn mt-4">Login</button>
-
-        </form>
-    </div>
+      <button type="submit" class="btn btn-primary mt-4">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
-import { PhEnvelope } from '@phosphor-icons/vue';
+import { PhEnvelope } from "@phosphor-icons/vue";
 
 export default {
-    components: {
-        PhEnvelope
-    }
-}
-
+  components: {
+    PhEnvelope,
+  },
+};
 </script>
