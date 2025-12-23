7fee888f195f229dd92ce3e1a94076d33399b14d
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-23 21:52:40 +0800

    restyle: register page

--- src/views/register-view.vue
+++ src/views/register-view.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="flex items-center p-4 justify-center h-[80vH] " v-motion-fade>
+  <div class="flex items-center p-4 justify-center h-[80vH]" v-motion-fade>
+    <form
+      action=""
+      class="flex flex-col gap-1 w-100 p-4 border border-gray-300 shadow-lg"
+    >
+      <h4 class="mb-4 font-bold">Register</h4>
+      <div class="flex flex-col gap-2">
+        <label for="" class="floating-label">
+          <span>Username</span>
+          <input type="text" class="input w-full" placeholder="Username" />
+        </label>
 
-        <form action="" class="flex flex-col gap-1 w-100 bg-gray-100 p-4 border border-gray-300">
-            <h4 class="mb-4 font-bold">Register</h4>
-            <label for="" class="floating-label">
-                <span>Username</span>
-                <input type="text" class="input w-full" placeholder="Username">
-            </label>
+        <label for="" class="floating-label">
+          <span>Email</span>
+          <input type="text" class="input w-full" placeholder="Email" />
+        </label>
 
-            <label for="" class="floating-label">
-                <span>Email</span>
-                <input type="text" class="input w-full mt-2" placeholder="Email">
-            </label>
+        <label for="" class="floating-label">
+          <span>Password</span>
+          <input type="text" class="input w-full" placeholder="Password" />
+        </label>
+      </div>
 
-
-            <label for="" class="floating-label">
-                <span>Password</span>
-                <input type="text" class="input w-full mt-2" placeholder="Password">
-
-            </label>
-
-
-            <button type="submit" class="btn mt-4">Register</button>
-
-        </form>
-    </div>
+      <button type="submit" class="btn btn-primary mt-4">Register</button>
+    </form>
+  </div>
 </template>
