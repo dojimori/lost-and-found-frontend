dadedd1abd3ba41e976d923544ee479f608cd113
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-23 22:09:19 +0800

    register: v-models

--- src/views/register-view.vue
+++ src/views/register-view.vue
@@ -1,29 +1,49 @@
 <template>
   <div class="flex items-center p-4 justify-center h-[80vH]" v-motion-fade>
     <form
-      action=""
+      @submit.prevent="submitHandler"
       class="flex flex-col gap-1 w-100 p-4 border border-gray-300 shadow-lg"
     >
       <h4 class="mb-4 font-bold">Register</h4>
       <div class="flex flex-col gap-2">
         <label for="" class="floating-label">
           <span>Name</span>
-          <input type="text" class="input w-full" placeholder="Name" />
+          <input
+            v-model="credentials.name"
+            type="text"
+            class="input w-full"
+            placeholder="Name"
+          />
         </label>
 
         <label for="" class="floating-label">
           <span>Email</span>
-          <input type="text" class="input w-full" placeholder="Email" />
+          <input
+            v-model="credentials.email"
+            type="text"
+            class="input w-full"
+            placeholder="Email"
+          />
         </label>
 
         <label for="" class="floating-label">
           <span>Password</span>
-          <input type="text" class="input w-full" placeholder="Password" />
+          <input
+            v-model="credentials.password"
+            type="text"
+            class="input w-full"
+            placeholder="Password"
+          />
         </label>
 
         <label for="" class="floating-label">
           <span>Confirm Password</span>
-          <input type="text" class="input w-full" placeholder="Confirm Password" />
+          <input
+            v-model="credentials.confirmPassword"
+            type="text"
+            class="input w-full"
+            placeholder="Confirm Password"
+          />
         </label>
       </div>
 
@@ -31,3 +51,24 @@
     </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      credentials: {
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      },
+    };
+  },
+
+  methods: {
+    async submitHandler() {
+      console.log(this.credentials);
+    },
+  },
+};
+</script>
