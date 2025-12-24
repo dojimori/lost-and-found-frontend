f3c7e794343c2632610013f2672d7a4196708490
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-24 16:03:14 +0800

    ...

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
-    }
+    },
 })
 
 export { api }
--- src/views/login-view.vue
+++ src/views/login-view.vue
@@ -1,30 +1,104 @@
 <template>
-  <div class="flex items-center p-4 justify-center h-[80vH]" v-motion-fade>
-    <form action="" class="flex flex-col w-100 p-4 border shadow-lg border-gray-300">
-      <h4 class="mb-4 font-bold">Login</h4>
-      <div class="flex flex-col gap-2">
-        <label for="" class="floating-label">
-          <span>Email</span>
-          <input type="text" class="input w-full" placeholder="Email" />
-        </label>
+    <div class="flex items-center justify-center p-12" v-motion-fade>
+        <form @submit.prevent="submitHandler" class="card bg-base-100 shadow-md w-full max-w-md">
+            <div class="card-body">
+                <h2 class="card-title text-2xl font-bold justify-center">Login</h2>
 
-        <label class="floating-label">
-          <span>Password</span>
-          <input type="text" class="input w-full" placeholder="Password" />
-        </label>
-      </div>
+                <div class="flex flex-col gap-1">
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text font-medium">Email</span>
+                        </div>
+                        <input v-model="credentials.email" type="email"
+                            class="input input-bordered w-full focus:input-primary" placeholder="johndoe@gmail.com"
+                            :disabled="isLoading" />
+                    </label>
 
-      <button type="submit" class="btn btn-primary mt-4">Login</button>
-    </form>
-  </div>
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text font-medium">Password</span>
+                        </div>
+                        <input v-model="credentials.password" type="password"
+                            class="input input-bordered w-full focus:input-primary" placeholder="••••••••"
+                            :disabled="isLoading" />
+                    </label>
+                </div>
+
+                <button type="submit" class="btn btn-primary w-full " :disabled="isLoading">
+                    <span v-if="isLoading" class="loading loading-spinner"></span>
+                    <span v-else>Login</span>
+                </button>
+
+                <div v-motion-fade v-if="showAlert"
+                    :class="['alert alert-soft', isError ? 'alert-error' : 'alert-success']">
+                    <svg v-if="isError" xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-5 w-5"
+                        fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-5 w-5" fill="none"
+                        viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <span>{{ responseMessage }}</span>
+                </div>
+
+                <!-- <p class="text-center text-sm mt-4 text-base-content/60">
+                    Already have an account?
+                    <router-link to="/login" class="link link-primary font-medium">Sign in</router-link>
+                </p> -->
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
-import { PhEnvelope } from "@phosphor-icons/vue";
+import { api } from "../helpers/api";
 
 export default {
-  components: {
-    PhEnvelope,
-  },
+    data() {
+        return {
+            isLoading: false,
+            isError: false,
+            showAlert: false,
+            responseMessage: "",
+            credentials: {
+                name: "",
+                email: "",
+                password: "",
+                confirmPassword: "",
+            },
+        };
+    },
+
+    methods: {
+        async submitHandler() {
+            try {
+                this.isLoading = true;
+                this.isError = false;
+                this.showAlert = false;
+                if (this.credentials.password != this.credentials.confirmPassword) {
+                    this.isError = true;
+                    this.responseMessage = "Password does not match.";
+                    return;
+                }
+
+                const response = await api.post("/auth/register", this.credentials);
+
+                // console.log(response);
+                this.responseMessage = response.data.message;
+            } catch (error) {
+                this.isError = true;
+                console.log(error);
+                const message =
+                    error.response.data.message || "Something wen't terribly wrong here.";
+                this.responseMessage = message;
+            } finally {
+                this.isLoading = false;
+                this.showAlert = true;
+            }
+        },
+    },
 };
 </script>
