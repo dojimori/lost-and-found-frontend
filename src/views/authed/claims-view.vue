46a08dd28f186fc408a62db6e730cbf3f46a02be
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-26 07:17:10 +0800

    redesign claims table

--- src/views/authed/claims-view.vue
+++ src/views/authed/claims-view.vue
@@ -27,10 +27,11 @@
     </div>
     <!-- <div class="divider"></div> -->
     <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100" v-motion-fade>
-        <table class="table">
+        <table class="table table-md">
             <!-- head -->
             <thead>
                 <tr>
+                    <th>#</th>
                     <th>Name</th>
                     <th>Item</th>
                     <th>Image</th>
@@ -41,41 +42,30 @@
             <tbody>
 
                 <tr>
-                    <td>Najid Badji Ali</td>
-                    <td>Wallet</td>
-                    <td><img src="https://scontent.fcgy1-1.fna.fbcdn.net/v/t39.30808-6/598274256_867114656251812_6662297040089705085_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeEyYvcKYon9WtZ0BIpckYWRWpVuJk4OEElalW4mTg4QSR-UQEzlkmKnTtVf-o3OeuEjmg4G-Tunx0-P_CCrLLi8&_nc_ohc=AZcgVEQu9wIQ7kNvwFTfsgm&_nc_oc=AdmmlJo7RQxwckjwxXeKS95nR7Ttfuv7uUk6IydEIxMtmMu1EEGLxZEp2MQ232l9hhA&_nc_zt=23&_nc_ht=scontent.fcgy1-1.fna&_nc_gid=n0E2T-ez710J8-tiYNDw_g&oh=00_Afnbt2VNgygaVjggaA9ihMmlpqkmzKpBD8JpfREDPCxbIg&oe=694BCDC0"
-                            class="max-w-[50px] rounded-md object-cover" />
-                    </td>
-                    <td>6/26/2026</td>
+                    <td>1</td>
                     <td>
+                        <div class="flex items-center gap-3">
+                            <div class="avatar">
+                                <div class="mask mask-squircle h-12 w-12">
+                                    <img src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                                        alt="Avatar Tailwind CSS Component" />
+                                </div>
+                            </div>
+                            <div>
+                                <div class="font-bold">Hart Hagerty</div>
+                                <div class="text-sm opacity-50">BSCS</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>Wallet</td>
+                    <td><img src="https://placehold.co/80x80" class="max-w-[50px] rounded-md object-cover" />
+                    </td>
+                    <td>June 26, 2025</td>
+                    <td class="flex gap-2">
                         <button class="btn btn-primary">Confirm</button>
+                        <button class="btn btn-secondary">Message</button>
                     </td>
                 </tr>
-
-                <tr>
-                    <td>Najid Badji Ali</td>
-                    <td>Wallet</td>
-                    <td><img src="https://scontent.fcgy1-1.fna.fbcdn.net/v/t39.30808-6/598274256_867114656251812_6662297040089705085_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeEyYvcKYon9WtZ0BIpckYWRWpVuJk4OEElalW4mTg4QSR-UQEzlkmKnTtVf-o3OeuEjmg4G-Tunx0-P_CCrLLi8&_nc_ohc=AZcgVEQu9wIQ7kNvwFTfsgm&_nc_oc=AdmmlJo7RQxwckjwxXeKS95nR7Ttfuv7uUk6IydEIxMtmMu1EEGLxZEp2MQ232l9hhA&_nc_zt=23&_nc_ht=scontent.fcgy1-1.fna&_nc_gid=n0E2T-ez710J8-tiYNDw_g&oh=00_Afnbt2VNgygaVjggaA9ihMmlpqkmzKpBD8JpfREDPCxbIg&oe=694BCDC0"
-                            class="max-w-[50px] rounded-md object-cover" />
-                    </td>
-                    <td>6/26/2026</td>
-                    <td>
-                        <button class="btn btn-primary">Confirm</button>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>Najid Badji Ali</td>
-                    <td>Wallet</td>
-                    <td><img src="https://scontent.fcgy1-1.fna.fbcdn.net/v/t39.30808-6/598274256_867114656251812_6662297040089705085_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeEyYvcKYon9WtZ0BIpckYWRWpVuJk4OEElalW4mTg4QSR-UQEzlkmKnTtVf-o3OeuEjmg4G-Tunx0-P_CCrLLi8&_nc_ohc=AZcgVEQu9wIQ7kNvwFTfsgm&_nc_oc=AdmmlJo7RQxwckjwxXeKS95nR7Ttfuv7uUk6IydEIxMtmMu1EEGLxZEp2MQ232l9hhA&_nc_zt=23&_nc_ht=scontent.fcgy1-1.fna&_nc_gid=n0E2T-ez710J8-tiYNDw_g&oh=00_Afnbt2VNgygaVjggaA9ihMmlpqkmzKpBD8JpfREDPCxbIg&oe=694BCDC0"
-                            class="max-w-[50px] rounded-md object-cover" />
-                    </td>
-                    <td>6/26/2026</td>
-                    <td>
-                        <button class="btn btn-primary">Confirm</button>
-                    </td>
-                </tr>
-
             </tbody>
         </table>
 
@@ -89,3 +79,15 @@
         </div>
     </div>
 </template>
+
+<script>
+import defPfp from '@/assets/def_pfp.jpg'
+
+export default {
+    data() {
+        return {
+            defPfp
+        }
+    }
+}
+</script>
