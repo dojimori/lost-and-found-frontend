0a05551a6a4401ba80248fd5e6e1ad5f95444cb2
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-25 11:17:30 +0800

    remove dummy posts

--- src/views/authed/feed-view.vue
+++ src/views/authed/feed-view.vue
@@ -33,7 +33,7 @@
     </dialog>
     <!-- <div class="divider"></div> -->
 
-    <div class="flex flex-row flex-wrap gap-8 items-center justify-center">
+    <div class="flex flex-row flex-wrap gap-4 items-center justify-center">
         <div v-for="item in lostItems" v-motion-fade
             class="border border-gray-400 card bg-base-200 rounded-none w-[250px] shadow-sm transition-all  duration-500 hover:shadow-md">
             <figure class="max-w-[280px] max-h-[200px]">
@@ -87,39 +87,6 @@ export default {
     data() {
         return {
             lostItems: [
-                // {
-                //     id: 1,
-                //     itemImage: 'https://scontent.fcgy1-1.fna.fbcdn.net/v/t39.30808-6/598274256_867114656251812_6662297040089705085_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeEyYvcKYon9WtZ0BIpckYWRWpVuJk4OEElalW4mTg4QSR-UQEzlkmKnTtVf-o3OeuEjmg4G-Tunx0-P_CCrLLi8&_nc_ohc=AZcgVEQu9wIQ7kNvwFTfsgm&_nc_oc=AdmmlJo7RQxwckjwxXeKS95nR7Ttfuv7uUk6IydEIxMtmMu1EEGLxZEp2MQ232l9hhA&_nc_zt=23&_nc_ht=scontent.fcgy1-1.fna&_nc_gid=n0E2T-ez710J8-tiYNDw_g&oh=00_Afnbt2VNgygaVjggaA9ihMmlpqkmzKpBD8JpfREDPCxbIg&oe=694BCDC0',
-                //     itemName: 'Wallet',
-                //     lastSeen: 'BLDG. 48',
-                //     status: 'not found',
-                //     user: {
-                //         profile: 'https://img.daisyui.com/images/profile/demo/yellingcat@192.webp',
-                //         name: 'Alexander Gabriel'
-                //     }
-                // },
-                // {
-                //     id: 2,
-                //     itemImage: 'https://scontent.fcgy1-3.fna.fbcdn.net/v/t39.30808-6/600215270_1872124793404801_1242119322575631170_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeFBcWREs8mj8wQILfpgrQcGv-DCcCfJmHy_4MJwJ8mYfOe5VE8YoJb1XlO2xYKBi1iesFoK8UEc6CS6eOTd3wJO&_nc_ohc=iQLBdsUI5u4Q7kNvwF68zNj&_nc_oc=AdnHDho1ipGX6ROVUW95FeZN_2DS_UlsOgVg5UbZQ8R_WiitlKHr-0Ehv4Vrv7oC2OU&_nc_zt=23&_nc_ht=scontent.fcgy1-3.fna&_nc_gid=mXg8C0_m0Kpw73VNBvgjhw&oh=00_AfmuEMnsKyGVfkWuVDAHLVgVJM0r9QdvDvICTZWgrNkmRg&oe=694BDF68',
-                //     itemName: 'Phone',
-                //     lastSeen: 'BLDG. 48',
-                //     status: 'found',
-                //     user: {
-                //         profile: 'https://img.daisyui.com/images/profile/demo/yellingcat@192.webp',
-                //         name: 'Rhudd Lawrence'
-                //     }
-                // },
-                // {
-                //     id: 3,
-                //     itemImage: 'https://scontent.fcgy1-1.fna.fbcdn.net/v/t39.30808-6/598274256_867114656251812_6662297040089705085_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeEyYvcKYon9WtZ0BIpckYWRWpVuJk4OEElalW4mTg4QSR-UQEzlkmKnTtVf-o3OeuEjmg4G-Tunx0-P_CCrLLi8&_nc_ohc=AZcgVEQu9wIQ7kNvwFTfsgm&_nc_oc=AdmmlJo7RQxwckjwxXeKS95nR7Ttfuv7uUk6IydEIxMtmMu1EEGLxZEp2MQ232l9hhA&_nc_zt=23&_nc_ht=scontent.fcgy1-1.fna&_nc_gid=n0E2T-ez710J8-tiYNDw_g&oh=00_Afnbt2VNgygaVjggaA9ihMmlpqkmzKpBD8JpfREDPCxbIg&oe=694BCDC0',
-                //     itemName: 'Wallet',
-                //     lastSeen: 'BLDG. 48',
-                //     status: 'not found',
-                //     user: {
-                //         profile: 'https://img.daisyui.com/images/profile/demo/yellingcat@192.webp',
-                //         name: 'Jc James'
-                //     }
-                // }
             ],
             itemName: "",
             description: "",
