07b3a650c20f425e6205567f20b927750c6d3a06
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-25 11:24:30 +0800

    landing page image

--- src/views/landing-page-view.vue
+++ src/views/landing-page-view.vue
@@ -1,14 +1,23 @@
 <template>
     <div class="hero  min-h-[86vh]">
+
         <div class="hero-content text-center">
+
+            <img :src="LandingPageImage" alt="">
             <div class="max-w-md animate-fade" v-motion-fade>
-                <h1 class="text-5xl font-bold">Something lost?</h1>
-                <p class="py-6">
+                <h1 class="text-5xl font-bold text-start">Something lost?</h1>
+                <p class="py-6 text-start">
                     A simple place where seekers and finders meet; bring lost items back home!
                 </p>
-                <router-link to="/register" class="btn btn-primary animate-bounce ">Get Started</router-link>
+                <router-link to="/register" class=" btn btn-primary animate-bounce ">Get
+                    Started</router-link>
             </div>
         </div>
     </div>
 
 </template>
+
+<script setup>
+import LandingPageImage from '../assets/landing-page.svg'
+
+</script>
