74332d6bf53bb1b1b4443b1af9b33af0a53dd386
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-27 21:34:43 +0800

    unclaim modal

--- src/views/authed/claims-view.vue
+++ src/views/authed/claims-view.vue
@@ -1,4 +1,18 @@
 <template>
+  <button class="btn" onclick="">open modal</button>
+<dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
+  <div class="modal-box">
+    <h3 class="text-lg font-bold">Are you sure you want to unclaim the item?</h3>
+    <div class="modal-action">
+        <button class="btn btn-soft btn-warning">Unclaim</button>
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn btn-soft btn-error">Cancel</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+
   <div class="flex flex-row gap-2 p-4 border-b border-gray-200">
     <div class="card card-border w-[300px] shadow-xs">
       <div class="card-body">
@@ -115,10 +129,7 @@
             <div class="flex items-center gap-3">
               <div class="avatar">
                 <div class="mask mask-squircle h-12 w-12">
-                  <img
-                    :src="defPfp"
-                    alt="Avatar Tailwind CSS Component"
-                  />
+                  <img :src="defPfp" alt="Avatar Tailwind CSS Component" />
                 </div>
               </div>
               <div>
@@ -127,14 +138,20 @@
               </div>
             </div>
           </td>
-          <td>{{ claimed.item.name  }}</td>
+          <td>{{ claimed.item.name }}</td>
           <td>
-             <img :src="`http://localhost:3000/public/${claimed.item?.image}`" class="w-[50px] h-[50px] rounded-md object-cover" :alt="claimed.item.name" />
+            <img
+              :src="`http://localhost:3000/public/${claimed.item?.image}`"
+              class="w-[50px] h-[50px] rounded-md object-cover"
+              :alt="claimed.item.name"
+            />
           </td>
           <td>June 26, 2025</td>
           <td class="flex gap-2">
             <button class="btn btn-primary">Message Founder</button>
-            <button class="btn btn-error btn-soft">Unclaim</button>
+            <button class="btn btn-error btn-soft" onclick="my_modal_5.showModal()">
+              Unclaim
+            </button>
           </td>
         </tr>
       </tbody>
@@ -159,24 +176,22 @@ export default {
   data() {
     return {
       defPfp,
-      myClaimedItems: null
+      myClaimedItems: null,
     };
   },
 
   methods: {
     async getMyClaimedItems() {
-        try {   
-            const response = await api.get('/claims/my-claimed-items');
-            this.myClaimedItems = response.data;
-            console.log(response)
-        } catch (error) {
-            
-        }
-    }
+      try {
+        const response = await api.get("/claims/my-claimed-items");
+        this.myClaimedItems = response.data;
+        console.log(response);
+      } catch (error) {}
+    },
   },
 
   mounted() {
     this.getMyClaimedItems();
-  }
+  },
 };
 </script>
